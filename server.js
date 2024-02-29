0a0815244c8b37dcc4544f2b125ebd19889d0258
nicolas572 <nicolas572@yahoo.com>
2024-02-28 16:57:35 -0800

    setting up express server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+
+//middleware 
+require('dotenv').config()
+app.use(express.json())
+const PORT = process.env.PORT || 3001
+
+//index route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+//listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
